e7cbef4a0f0af74fe1e899fc4595f2e8c6361e0a
Felipe Lagos <felagossanchez@gmail.com>
2021-10-03 21:59:15 -0300

    screens app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
+import { StackNavigation } from './navigation/StackNavigation'
 
 export const App = () => {
 	return (
 		<NavigationContainer>
-
+			<StackNavigation />
 		</NavigationContainer>
 	)
 }
---
+++ src/navigation/StackNavigation.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { createStackNavigator, StackNavigationOptions } from "@react-navigation/stack";
+import { HomeScreen } from "../screens/HomeScreen";
+import { DetailScreen } from "../screens/DetailScreen";
+
+enum StackScreens {
+	HOME = "HomeScreen",
+	DETAIL = "DetailScreen"
+}
+
+const Stack = createStackNavigator();
+
+const screenOptions: StackNavigationOptions = {
+	headerShown: false,
+	cardStyle: {
+		backgroundColor: "white"
+	}
+}
+
+export const StackNavigation = () => {
+	
+	return(
+		<Stack.Navigator screenOptions={screenOptions}>
+			<Stack.Screen name={StackScreens.HOME} component={HomeScreen} />
+			<Stack.Screen name={StackScreens.DETAIL} component={DetailScreen} />
+		</Stack.Navigator>
+	);
+
+}
