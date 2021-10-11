beb42429f0c7464af90e34744d0cd083be71eeab
Felipe Lagos <felagossanchez@gmail.com>
2021-10-10 21:29:00 -0300

    component movie

---
+++ src/components/MoviePoster.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Image, StyleSheet, View } from 'react-native'
+import { Movie } from '../models/movie.model'
+import { BASE_URL_IMAGE } from '@env';
+
+interface Props {
+	movie: Movie
+}
+
+export const MoviePoster = ({ movie }: Props) => {
+	return (
+		<View style={styles.container}>
+			<View style={styles.imageCover}>
+				<Image source={{ uri: `${BASE_URL_IMAGE}${movie.poster_path}` }} style={styles.image} />
+			</View>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		width: 300,
+		height: 420
+	},
+	imageCover: {
+		flex: 1,
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.24,
+		shadowRadius: 3.84,
+		elevation: 10
+	},
+	image: {
+		flex: 1,
+		borderRadius: 18
+	}
+})
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-import { useQuery } from 'react-query'
+import { View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { Loader } from '../components/Loader'
+import { MoviePoster } from '../components/MoviePoster'
+import { useFetch } from '../hooks/useFetch'
+import { ResponseMovie } from '../models/movie.model'
 import { getMoviesNowPlaying } from '../services/movies.service'
 
 export const HomeScreen = () => {
+	const { top } = useSafeAreaInsets()
 
-	const { data } = useQuery("moviesNowPlaying", getMoviesNowPlaying)
+	const { data, isLoading } = useFetch("moviesNowPlaying", getMoviesNowPlaying)
+	if (isLoading) return <Loader />
+
+	const { results: movies } = data
 
 	return (
-		<View>
-			<Text></Text>
+		<View style={{ marginTop: top }}>
+			<MoviePoster movie={movies[0]} />
 		</View>
 	)
 }
-
