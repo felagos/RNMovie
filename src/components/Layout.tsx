a00dc192df31ae1135c2189b2b4037e528a8565e
Felipe Lagos <felagossanchez@gmail.com>
2021-10-11 13:22:35 -0300

    update layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,14 +3,14 @@ import { View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 interface Props {
-	children: JSX.Element
+	children: JSX.Element | JSX.Element[]
 }
 
 export const Layout = ({ children }: Props) => {
 	const { top } = useSafeAreaInsets();
 
 	return (
-		<View style={{ marginTop: top }}>
+		<View style={{ marginTop: top + 20 }}>
 			{children}
 		</View>
 	);
